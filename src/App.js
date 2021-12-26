c16210301564531285d3454cb65fff8624b767c0
Fistlol <ars-abz@mail.ru>
2021-12-27 02:55:00 +0600

    online store

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Header from './components/Header/Header';
+import ProductListing from './components/ProductListing/ProductListing';
+import ProductDetail from './components/ProductDetail/ProductDetail';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" exact element={<ProductListing />} />
+          <Route path="/product/:productId" exact element={<ProductDetail />} />
+          <Route path="*" exact element={<ProductListing />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
